74876a1bc5a0b49192e0441a50e30ad0d6db587a
annie tran <nntrn@me.com>
2019-06-06 22:44:43 -0500

    Updates

--- precache-manifest.cc3f2e49382cd5de6cb046a818cb3960.js
+++ precache-manifest.cc3f2e49382cd5de6cb046a818cb3960.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e1670279c6a687b0eb7735469a0ffba6",
+    "revision": "bd7dad421de7179e4cd202ad8cfbff8a",
     "url": "/react-mini-projects/index.html"
   },
   {
-    "revision": "f82a7b5ea4edb24505bf",
-    "url": "/react-mini-projects/static/css/main.afe07d31.chunk.css"
+    "revision": "447a0bb0df53ddfd16c9",
+    "url": "/react-mini-projects/static/css/main.f82e1c29.chunk.css"
   },
   {
     "revision": "6d06cd310fad007f8ce6",
     "url": "/react-mini-projects/static/js/2.35fc07b0.chunk.js"
   },
   {
-    "revision": "f82a7b5ea4edb24505bf",
-    "url": "/react-mini-projects/static/js/main.fbe2d57e.chunk.js"
+    "revision": "447a0bb0df53ddfd16c9",
+    "url": "/react-mini-projects/static/js/main.5645ab07.chunk.js"
   },
   {
     "revision": "ccb13ef055f605caeb2f",
